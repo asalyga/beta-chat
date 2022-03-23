d9ad4e0147a4f33c865fdbdc015426cc3631250f
Aleksander Sałyga <aleksander.salyga@gmail.com>
2022-03-23 14:54:27 +0100

    Add sessions create and destroy method'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
 
-  def logged_user
-    @logged_user ||= User.find(session[:user_id]) if session[:user_id]
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
-    !!logged_user
+    !!current_user
   end
 
   def require_user
-    unless logged_in?
-      flash[:error] = 'You must log in first'
+    if !logged_in?
+      flash[:error] = "You must be logged in to perform that action"
       redirect_to login_path
     end
   end
-
 end
